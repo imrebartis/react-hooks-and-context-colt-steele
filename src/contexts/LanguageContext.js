f92e1371cfd8fd4f032c84a70e13e3de71f5939c
Imre Bartis <imre.bartis@siili.com>
2019-11-24 17:23:32 +0200

    consume multiple contexts with hooks

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -22,9 +22,3 @@ export class LanguageProvider extends Component {
     );
   }
 }
-
-export const withLanguageContext = Component => props => (
-  <LanguageContext.Consumer>
-    {value => <Component LanguageContext={value} {...props} />}
-  </LanguageContext.Consumer>
-);
